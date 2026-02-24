948fef10e1cfa747d821a5bc078a3f040d76ead6
Ishan Subedi <subediishan10@gmail.com>
2026-02-24 09:45:56 -0800

    changed navbar and footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import FooterSkeleton from "../skeleton/FooterSkeleton";
-
+import { Link } from "react-router-dom";
 function Footer() {
   const [loading, setLoading] = useState(true);
 
@@ -14,12 +14,18 @@ function Footer() {
   ) : (
     <div>
       <footer className="footer footer-horizontal footer-center text-base-content rounded p-10">
-        <nav className="grid grid-flow-col gap-4">
-          <a className="link link-hover">About us</a>
-          <a className="link link-hover">Contact</a>
-          <a className="link link-hover">Jobs</a>
-          <a className="link link-hover">Press kit</a>
+        <nav className="grid grid-flow-col gap-4 text-base-content">
+          <Link to="/about" className=" hover:text-pink-500 cursor-pointer">
+            About us
+          </Link>
+          <Link to="/contact" className="hover:text-pink-500 cursor-pointer">
+            Contact
+          </Link>
+          <Link to="/categories" className="hover:text-pink-500 cursor-pointer">
+            Categories
+          </Link>
         </nav>
+
         <nav>
           <div className="grid grid-flow-col gap-4">
             <a>
@@ -59,8 +65,8 @@ function Footer() {
         </nav>
         <aside>
           <p>
-            Copyright © {new Date().getFullYear()} - All right reserved by ACME
-            Industries Ltd
+            Copyright © {new Date().getFullYear()} - All right reserved by
+            BOOKSTORE Pvt. Ltd.
           </p>
         </aside>
       </footer>
